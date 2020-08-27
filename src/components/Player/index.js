050bc3e7b263243aa32ab0801c5aec3da07d73a8
Enmanuel Jarquin <enmisac@gmail.com>
2020-08-26 18:55:07 -0600

    agreagr estilos al player

--- src/components/Player/index.js
+++ src/components/Player/index.js
@@ -1,24 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import Sound from "react-sound";
 import PlayButton from "../PlayButton";
 import Progress from "../ProgressBar";
 import { MdSkipPrevious, MdSkipNext } from "react-icons/md";
 import "./styles.css";
+import { formatMilliseconds } from "../../utilities/formatMilliseconds";
 
-const Player = () => {
+const Player = ({ src }) => {
+  const [progress, setProgress] = useState({
+    position: 0,
+  });
+
+  const handleSongPlaying = (audio) => {
+    setProgress({
+      elapsed: formatMilliseconds(audio.position),
+      total: formatMilliseconds(audio.duration),
+      position: audio.position / audio.duration,
+    });
+  };
+
+  const onPausePlay = () => {
+    if (!src) return;
+
+    // togglePlay()
+  };
+
   return (
     <div className="player">
       <div className="player-actions">
         <button className="player-action">
           <MdSkipPrevious />
         </button>
-        <PlayButton onClick={() => alert(123)} playing={false} />
+        <PlayButton onClick={onPausePlay} playing={false} />
         <button className="player-action">
           <MdSkipNext />
         </button>
+        <Sound
+          url={""}
+          playStatus={Sound.status.STOPPED}
+          onPlaying={handleSongPlaying}
+          onFinishedPlaying={() => {}}
+        />
       </div>
       <div className="player-progress">
-        <Progress progress={{}} />
+        <Progress progress={progress} />
       </div>
     </div>
   );
